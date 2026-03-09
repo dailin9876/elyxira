23b0d5830f95b22cca191882896240c15bef94e3
dailin9876 <dailin9876@gmail.com>
2026-03-09 23:18:01 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,9 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-//export default defineConfig({
-export default defineConfig(({ command }) => {  
-  return{
+export default defineConfig({
   plugins: [vue()],
-  //base: '/elyxira/',
-  base: command === 'build' ? '/elyxira/' : '/',
+  base: '/elyxira/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
@@ -16,5 +13,5 @@ export default defineConfig(({ command }) => {
   build: {
     // 建议：确保 assetsDir 保持默认或显式设置
     assetsDir: 'assets',
-  }}
+  }
 })
